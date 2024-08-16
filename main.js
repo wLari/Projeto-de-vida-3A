14b29b62680b0fc431df82e61fee63bbe77fbcc4
wLari <163433120+wLari@users.noreply.github.com>
2024-08-16 12:24:44 -0300

    cafe

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+
+for(let i=0;1 <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("attivo");
+
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
