d9c8caaf768e9b95843b1e5da6f573221ed22208
wLari <163433120+wLari@users.noreply.github.com>
2024-08-23 12:20:30 -0300

    cofe

--- main.js
+++ main.js
@@ -5,8 +5,23 @@ for(let i=0;1 <botoes.length;i++){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("attivo");
+            textos[j].classList.remove("ativo");
 
         }
+
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = tempoFinal/1000;
+
 }
